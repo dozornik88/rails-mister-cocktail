ba77c866dd9def29b317f0dfdf4161686da7ba92
Sergejs Dementjevs <dozornik88@gmail.com>
2019-12-19 20:24:18 +0100

    test for mistakesss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,4 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
-import 'bootstrap';
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
